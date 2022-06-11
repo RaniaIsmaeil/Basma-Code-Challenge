ce50c941dcb8e5649a14793a75b365176937d30f
RaniaIsmaeil <82700783+RaniaIsmaeil@users.noreply.github.com>
2022-06-11 12:31:27 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import Application from "../components/Application/Application";
 import Arrow from "../components/Arrow/Arrow";
 import CodeChallenge from "../components/CodeChallenge/CodeChallenge";
